f51ef02548fd95490dafe72436cd6dae55dfab2c
Hein Rutjes <hrutjes@gmail.com>
2019-04-07 10:34:28 +0200

    Prevent screen from turning off when app is in front.

--- android/app/src/main/java/com/tvbrowser/MainActivity.java
+++ android/app/src/main/java/com/tvbrowser/MainActivity.java
@@ -1,16 +1,27 @@
 package com.tvbrowser;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
         return "TVBrowser";
     }
-}
 
+    /**
+     * Prevent screen from turning off when app is in front.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+}
